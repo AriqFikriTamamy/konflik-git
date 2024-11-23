cb6fffd6de0a85aa1bee5c04bd8878de62e8b2cb
Ariq Fikri Tamamy <ariqrick@gmail.com>
2024-11-24 02:06:09 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-// Endpoint untuk register user
-app.post('/register', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk register user
-    res.send('User registered');
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
 });
 
-// Endpoint untuk login user
-app.post('/login', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk login user
-    res.send('User logged in');
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
 });
